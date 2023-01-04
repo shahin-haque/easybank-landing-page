6516f67b2f34fbbdc74af4678a20d59f1583f197
Shahin <shahinhaque07@gmail.com>
2023-01-04 20:04:32 +0100

    hero style added

--- components/Hero.js
+++ components/Hero.js
@@ -1,28 +1,32 @@
 import Image from 'next/image';
 
 import desktopBgIntro from '../public/bg-intro-desktop.svg';
+import mobileBgIntro from '../public/bg-intro-mobile.svg';
 import mockups from '../public/image-mockups.png';
 
 const Hero = () => {
   return (
     <section className="hero">
-      <div className="illustration-mockups">
-        <picture className="illustration">
-          <source media="(max-width:46rem)" srcSet="/bg-intro-mobile.svg" />
-          <source media="(min-width:47rem)" srcSet="/bg-intro-desktop.svg" />
-          <Image src={desktopBgIntro} alt="illustration" />
-        </picture>
-        <Image className="mockups" src={mockups} alt="mockups" />
+      <div className="container">
+        <div className="illustration-mockups">
+          <picture className="illustration">
+            <source media="(max-width:46rem)" srcSet="/bg-intro-mobile.svg" />
+            <source media="(min-width:47rem)" srcSet="/bg-intro-desktop.svg" />
+            <Image src={desktopBgIntro} alt="illustration" />
+          </picture>
+          <Image className="mockups" src={mockups} alt="mockups" />
+        </div>
+
+        <article className="hero-info">
+          <h1>Next generation digital banking</h1>
+          <p>
+            Take your financial life online. Your Easybank account will be a
+            one-stop-shop for spending, saving, budgeting, investing, and much
+            more.
+          </p>
+          <button className="btn">Request Invite</button>
+        </article>
       </div>
-      <article className="hero-info">
-        <h1>Next generation digital banking</h1>
-        <p>
-          Take your financial life online. Your Easybank account will be a
-          one-stop-shop for spending, saving, budgeting, investing, and much
-          more.
-        </p>
-        <button className="btn">Request Invite</button>
-      </article>
     </section>
   );
 };
